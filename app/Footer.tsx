523bda2227e0fbf1931995d4cf5f99a99bd4c0d4
praveens649 <praveenv649@gmail.com>
2025-01-05 19:59:53 +0530

    fix:minor changes

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-950 text-white py-6 mt-10">
       <div className="container mx-auto px-6 text-center">
-        <p className="text-sm mb-2">&copy; {new Date().getFullYear()} Flimfy. All rights reserved.</p>
+        <p className="text-sm mb-2">&copy; {new Date().getFullYear()} Filmfy. All rights reserved.</p>
         <div className="flex justify-center space-x-6">
           <Link href="/About" className="text-gray-400 hover:text-white">About</Link>
           <Link href="/" className="text-gray-400 hover:text-white">Contact</Link>
@@ -13,7 +13,7 @@ const Footer = () => {
           <Link href="/" className="text-gray-400 hover:text-white">Terms of Service</Link>
         </div>
         <div className="mt-4">
-          <p className="text-xs text-gray-400">Flimfy is a platform dedicated to helping you discover the best movies and organize your watchlist.</p>
+          <p className="text-xs text-gray-400">Filmfy is a platform dedicated to helping you discover the best movies and organize your watchlist.</p>
         </div>
       </div>
     </footer>
